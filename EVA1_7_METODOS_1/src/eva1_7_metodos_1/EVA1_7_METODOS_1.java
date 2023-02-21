61eb81fcaade35f94789466b3a995719c2d588f3
carlosgr55 <124015981+carlosgr55@users.noreply.github.com>
2023-02-21 09:28:02 -0600

    Add files via upload

---
+++ EVA1_7_METODOS_1/src/eva1_7_metodos_1/EVA1_7_METODOS_1.java
@@ -0,0 +1,14 @@
+package eva1_7_metodos_1;
+
+public class EVA1_7_METODOS_1 {
+    
+    public static void myMethod(){
+        System.out.println("BAD GYAL !!!!");
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        myMethod();
+    }
+
+}
