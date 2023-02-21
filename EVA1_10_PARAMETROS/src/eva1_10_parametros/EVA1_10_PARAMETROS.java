bf15a3403be6cd214398d4307f4239785c25454a
carlosgr55 <124015981+carlosgr55@users.noreply.github.com>
2023-02-21 09:50:54 -0600

    Add files via upload

---
+++ EVA1_10_PARAMETROS/src/eva1_10_parametros/EVA1_10_PARAMETROS.java
@@ -0,0 +1,16 @@
+package eva1_10_parametros;
+
+public class EVA1_10_PARAMETROS {
+
+    public static void nPrintln(String message, int n){
+        for(int i=0; i<n;i++){
+            System.out.println(message);
+        }
+    
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        nPrintln("MOTOMAMI",5);
+    }
+
+}
